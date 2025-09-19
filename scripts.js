8dbcfe66bad39424ff17ead0ca50e0d5a252d3a0
GabeSchafferr <ggabeschaffer10@gmail.com>
2025-09-19 09:48:07 -0300

    ajuste animação frutas e correção de bugs

--- scripts.js
+++ scripts.js
@@ -6,5 +6,17 @@ let count = list.length;
 let active = 0;
 
 next.onclick = () => {
-    console.log("opa");
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+
+    active = active >= count -1 ? 0 : active + 1;
+    list[active].classList.add('active');
+}
+
+prev.onclick = () => {
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+
+    active = active <= 0 ? count -1 : active - 1;
+    list[active].classList.add('active');
 }
