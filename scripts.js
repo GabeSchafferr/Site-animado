47b26879828e0bfaac30370b5d866f67e219e5d7
GabeSchafferr <ggabeschaffer10@gmail.com>
2025-09-19 09:43:49 -0300

    Finalização site animado frutas

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let list = document.querySelectorAll('.item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let count = list.length;
+let active = 0;
+
+next.onclick = () => {
+    console.log("opa");
+}
